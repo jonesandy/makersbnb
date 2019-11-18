80bad1cdeb885b167ecbc57e09efdc8312ba571a
elfi yang <elfiyang16@gmail.com>
2019-11-18 15:29:41 +0000

    set-up

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MakersBnB
+
+*create database*
+- Please start by forking this repo.
+- In a terminal run `psql`
+- Create the following databases with:
+```
+CREATE DATABASE makers_bnb_dev;
+CREATE DATABASE makers_bnb_test;
+```
+<!-- - `rackup`
+- Using a web browser visit the relevant localhost -->
---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'data_mapper'
+require 'database_cleaner'
+
+Capybara.app = MakersBnb
+
+
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+  #
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+  #
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+end
