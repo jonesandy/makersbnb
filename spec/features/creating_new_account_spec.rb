7b29dafc1b552640434502e27bd4ea348eeb681a
Ed Courage <edcourage@live.com>
2019-11-20 11:17:27 +0000

    refactor - html working as it should

--- spec/features/creating_new_account_spec.rb
+++ spec/features/creating_new_account_spec.rb
@@ -16,23 +16,10 @@ feature 'Account' do
       end
     end
 
-    it "new account is created with correct details" do
+    it "does not create two account with the same email" do
+
+      Account.create(email:'darthvader@empireplc.com')
       visit '/'
-
-      within '.sign_up' do
-        fill_in 'email', with: 'darthvader@empireplc.com'
-        fill_in 'password', with: 'Iamyoufather123'
-        fill_in 'first_name', with: 'Darth'
-        fill_in 'last_name', with: 'Vader'
-        click_on 'Create Account'
-      end
-
-      within '.title' do
-        expect(page).to have_text("Welcome Darth")
-      end
-
-      click_on 'Home'
-
       within '.sign_up' do
         fill_in 'email', with: 'darthvader@empireplc.com'
         fill_in 'password', with: 'Iamyoufather123'
@@ -40,9 +27,10 @@ feature 'Account' do
         fill_in 'last_name', with: 'Vader'
         click_on 'Create Account'
         expect(page).to have_text("Email Already In Use")
+
       end
 
-      
+
     end
 
 
