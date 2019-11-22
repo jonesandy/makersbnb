4d3252c36387c34b12740191893d99ff5d969105
elfi yang <elfiyang16@gmail.com>
2019-11-22 15:12:04 +0000

    password being encrypted

--- lib/Account.rb
+++ lib/Account.rb
@@ -1,15 +1,29 @@
 require 'data_mapper'
+require 'bcrypt'
 
 class Account
   include DataMapper::Resource
+  include BCrypt
+
+  attr_reader :password
 
   property :id, Serial
   property :first_name, String
   property :last_name, String
   property :email, String, :unique => true
-  property :password, String, :length => 6..25
+  property :encrypted_password, String,   length: 60
 
   has n, :listing
   has n, :booking
+
+  def password=(password)
+      @password = password
+      self.encrypted_password = BCrypt::Password.create(password)
+  end
+
+  def right_password?(email:, password:)
+    account = Account.first(email: email)
+    BCrypt::Password.new(account.encrypted_password) == password
+  end
 
 end
