365065c5231f6468c9ba0da8a97db5dfbba712a1
Sara <srancati0@gmail.com>
2019-11-22 12:29:35 +0000

    working on dates

---
+++ public/js/interface.js
@@ -0,0 +1,26 @@
+var blocked_period = [ { start: "02/21/2012", end: "02/22/2012" }, { start: "03/21/2012", end: "03/22/2012" } ];
+
+console.log(blocked_period);
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+$(function() {
+$( "#start_date_booking" ).datepicker();
+});
+});
+
+// $(document).ready(function() {
+// $('#start_date').datepicker({
+//     beforeShowDay: function(date){
+//         if(date == 12/01/2020){
+//             return [false];
+//         }
+//         return [true];
+//     }
+// });
